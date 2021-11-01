b2a476b3413f17a79eb8ddb70a7a86e00bd10e6a
juangraydev <jwsabala@gmail.com>
2021-11-01 10:33:21 +0800

    latest

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post('/api/', (req, res) => {
 // --> Add this
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
 res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
