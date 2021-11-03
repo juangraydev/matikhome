74abb2af4a1c4db2f533327ea899df14ac93c8a9
juangraydev <jwsabala@gmail.com>
2021-11-03 17:27:41 +0800

    dir

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get("/api",  function(req, res) {
 // --> Add this
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join('client/build', '/index.html'));
+    res.sendFile(path.join(__dirnam, 'client/build', '/index.html'));
   });
 }
 
